b4791e4d20b2d3b87d4ef901198c191bb32f5e0b
Michael Barton <mike@weirdlooking.com>
2013-02-25 23:13:11 -0800

    swift working

--- swiftcellerate/middleware.py
+++ swiftcellerate/middleware.py
@@ -14,13 +14,14 @@ class OS(object):
     close = os.close
     unlink = os.unlink
     write = fileio.os_write
+    read = fileio.os_read
 
 def filter_factory(global_conf, **local_conf):
     def app_returner(app):
         swift.common.wsgi.wsgi = wsgi
         swift.common.utils.get_hub = get_hub
         swift.obj.server.tpool = tpool
-#        swift.obj.server.os = OS
+        swift.obj.server.os = OS
 #        __builtins__['file'] = fileio.File
 #        __builtins__['open'] = fileio.File
 #        os.read = fileio.os_read
